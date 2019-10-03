a5afc1d6abf49b6d0623436cac252fdea2f939da
ame2 <https://git01.codeplex.com/jscriptsuite>
2019-10-03 12:51:58 +0200

    move Cotrol type in tagName

--- src/Modules/Webs/UI/WebPane.html.cs
+++ src/Modules/Webs/UI/WebPane.html.cs
@@ -1,4 +1,5 @@
 ﻿using SharePoint.Explorer.Modules.Nodes;
+using JScriptSuite.DataBinding;
 using JScriptSuite.JScriptLib.UI.Controls.DataBinding;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Text;
 
 namespace SharePoint.Explorer.Modules.Webs.UI
 {
+    [Control("<div style=\"overflow:auto;height:100%;width:100%\"></div>")]
     class WebPane : UserControl<WebNode>
     {
     }
